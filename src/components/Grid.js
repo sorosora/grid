e443cbdaa3931adef3d6901153750faee9dfb1a6
sorosora <jackson90295@gmail.com>
2019-01-07 16:25:49 +0800

    vertical-gutter --> verticalGutter

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,12 +7,12 @@ import Col from './Col';
 const Grid = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin-top: ${({ 'vertical-gutter': [desktop = ''] = [] }) => getHalfMeasure(`-${desktop}`)};
-  margin-bottom: ${({ 'vertical-gutter': [desktop = ''] = [] }) => getHalfMeasure(`-${desktop}`)};
+  margin-top: ${({ verticalGutter: [desktop = ''] = [] }) => getHalfMeasure(`-${desktop}`)};
+  margin-bottom: ${({ verticalGutter: [desktop = ''] = [] }) => getHalfMeasure(`-${desktop}`)};
   margin-left: ${({ gutter: [desktop = gridGutter] = [] }) => getHalfMeasure(`-${desktop}`)};
   margin-right: ${({ gutter: [desktop = gridGutter] = [] }) => getHalfMeasure(`-${desktop}`)};
   
-  ${({ gutter: [gutter = gridGutter] = [], 'vertical-gutter': [verticalGutter = ''] = [], 'show-grid': showGrid }) => (showGrid ? css`
+  ${({ gutter: [gutter = gridGutter] = [], verticalGutter: [verticalGutter = ''] = [], 'show-grid': showGrid }) => (showGrid ? css`
     position: relative;
     
     &:after {
@@ -39,19 +39,19 @@ const Grid = styled.div`
   ` : '')}
   
   ${Col} {
-    padding-top: ${({ 'vertical-gutter': [desktop = ''] = [] }) => getHalfMeasure(desktop)};
-    padding-bottom: ${({ 'vertical-gutter': [desktop = ''] = [] }) => getHalfMeasure(desktop)};
+    padding-top: ${({ verticalGutter: [desktop = ''] = [] }) => getHalfMeasure(desktop)};
+    padding-bottom: ${({ verticalGutter: [desktop = ''] = [] }) => getHalfMeasure(desktop)};
     padding-left: ${({ gutter: [desktop = gridGutter] = [] }) => getHalfMeasure(desktop)};
     padding-right: ${({ gutter: [desktop = gridGutter] = [] }) => getHalfMeasure(desktop)};
   }
   
   ${media.tablet} {
-    margin-top: ${({ 'vertical-gutter': [, tablet = ''] = [] }) => getHalfMeasure(`-${tablet}`)};
-    margin-bottom: ${({ 'vertical-gutter': [, tablet = ''] = [] }) => getHalfMeasure(`-${tablet}`)};
+    margin-top: ${({ verticalGutter: [, tablet = ''] = [] }) => getHalfMeasure(`-${tablet}`)};
+    margin-bottom: ${({ verticalGutter: [, tablet = ''] = [] }) => getHalfMeasure(`-${tablet}`)};
     margin-left: ${({ gutter: [, tablet = ''] = [] }) => getHalfMeasure(`-${tablet}`)};
     margin-right: ${({ gutter: [, tablet = ''] = [] }) => getHalfMeasure(`-${tablet}`)};
     
-    ${({ gutter: [, gutter = ''] = [], 'vertical-gutter': [, verticalGutter = ''] = [], 'show-grid': showGrid }) => (showGrid ? css`
+    ${({ gutter: [, gutter = ''] = [], verticalGutter: [, verticalGutter = ''] = [], 'show-grid': showGrid }) => (showGrid ? css`
       &:after {
         margin-top: ${getHalfMeasure(verticalGutter)};
         margin-bottom: ${getHalfMeasure(verticalGutter)};
@@ -69,20 +69,20 @@ const Grid = styled.div`
    ` : '')}
     
     ${Col} {
-      padding-top: ${({ 'vertical-gutter': [, tablet = ''] = [] }) => getHalfMeasure(tablet)};
-      padding-bottom: ${({ 'vertical-gutter': [, tablet = ''] = [] }) => getHalfMeasure(tablet)};
+      padding-top: ${({ verticalGutter: [, tablet = ''] = [] }) => getHalfMeasure(tablet)};
+      padding-bottom: ${({ verticalGutter: [, tablet = ''] = [] }) => getHalfMeasure(tablet)};
       padding-left: ${({ gutter: [, tablet = ''] = [] }) => getHalfMeasure(tablet)};
       padding-right: ${({ gutter: [, tablet = ''] = [] }) => getHalfMeasure(tablet)};
     }
   }
   
   ${media.phone} {
-    margin-top: ${({ 'vertical-gutter': [, , phone = ''] = [] }) => getHalfMeasure(`-${phone}`)};
-    margin-bottom: ${({ 'vertical-gutter': [, , phone = ''] = [] }) => getHalfMeasure(`-${phone}`)};
+    margin-top: ${({ verticalGutter: [, , phone = ''] = [] }) => getHalfMeasure(`-${phone}`)};
+    margin-bottom: ${({ verticalGutter: [, , phone = ''] = [] }) => getHalfMeasure(`-${phone}`)};
     margin-left: ${({ gutter: [, , phone = ''] = [] }) => getHalfMeasure(`-${phone}`)};
     margin-right: ${({ gutter: [, , phone = ''] = [] }) => getHalfMeasure(`-${phone}`)};
     
-    ${({ gutter: [, , gutter = ''] = [], 'vertical-gutter': [, , verticalGutter = ''] = [], 'show-grid': showGrid }) => (showGrid ? css`
+    ${({ gutter: [, , gutter = ''] = [], verticalGutter: [, , verticalGutter = ''] = [], 'show-grid': showGrid }) => (showGrid ? css`
       &:after {
         margin-top: ${getHalfMeasure(verticalGutter)};
         margin-bottom: ${getHalfMeasure(verticalGutter)};
@@ -100,8 +100,8 @@ const Grid = styled.div`
     ` : '')}
     
     ${Col} {
-      padding-top: ${({ 'vertical-gutter': [, , phone = ''] = [] }) => getHalfMeasure(phone)};
-      padding-bottom: ${({ 'vertical-gutter': [, , phone = ''] = [] }) => getHalfMeasure(phone)};
+      padding-top: ${({ verticalGutter: [, , phone = ''] = [] }) => getHalfMeasure(phone)};
+      padding-bottom: ${({ verticalGutter: [, , phone = ''] = [] }) => getHalfMeasure(phone)};
       padding-left: ${({ gutter: [, , phone = ''] = [] }) => getHalfMeasure(phone)};
       padding-right: ${({ gutter: [, , phone = ''] = [] }) => getHalfMeasure(phone)};
     }
