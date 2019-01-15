901eb2220498d7cec74d3e1423c06bf362f4a7db
sorosora <jackson90295@gmail.com>
2019-01-15 17:44:27 +0800

    add helps getCss

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,8 @@
+import { css } from 'styled-components';
+import getValue from 'get-value';
+import { media } from './breakpoints';
+import { breakpoints, defaultGrid } from './defaultValue';
+
 const isNaN = (value) => {
   const n = Number(value);
   const nSelf = n;
@@ -22,4 +27,47 @@ const percentage = (value) => {
   return `${value * 100}%`;
 };
 
-export { isNaN, getHalfMeasure, percentage };
+const getGridSetting = (props, cssPropsMap) => {
+  // custom grid setting
+  let customGrid = {};
+  Object.values(cssPropsMap).forEach(prop => {
+    if (props[prop]) customGrid[prop] = props[prop];
+  });
+  // theme grid setting
+  const themeGrid = getValue(props, 'theme.grid', { default: {} });
+  // combine all grid settings
+  return {...defaultGrid, ...themeGrid, ...customGrid};
+};
+
+const getCssStyle = (cssPropsMap, gridSetting, breakpointKey) =>
+  Object.keys(cssPropsMap).map(cssProp => {
+    const cssValue = getValue(gridSetting, `${cssPropsMap[cssProp]}.${breakpointKey}`, '');
+    if (!cssValue) return;
+    return `
+      ${cssProp}: ${cssValue};
+    `
+  }).join('');
+
+// get css style with different breakpoints
+const getCssWithBreakpoints = (props, cssPropsMap) => {
+  const gridSetting = getGridSetting(props, cssPropsMap);
+  return Object.keys(breakpoints).map((breakpoint, key) => {
+    const cssStyle = getCssStyle(cssPropsMap, gridSetting, key);
+    return css`
+      /* desktop-first */
+      ${cssStyle && breakpoint !== 'desktop' ? `${media[breakpoint]} {` : ''};
+      ${cssStyle}
+      ${cssStyle && breakpoint !== 'desktop' ? '}' : ''};
+    `
+  });
+};
+
+/**
+ * @param props
+ * @param {object} cssPropsMap - {cssProp: gridSettingProp}
+ * @param {css} base - base css style
+ * @returns {*[]}
+ */
+const getCss = (props, cssPropsMap, base = css``) => [].concat(base, getCssWithBreakpoints(props, cssPropsMap));
+
+export { isNaN, getHalfMeasure, percentage, getCss };
