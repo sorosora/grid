7a812634949c7ca03799f1c4d12799be23cd4e1b
sorosora <jackson90295@gmail.com>
2019-01-16 13:57:34 +0800

    modify GridWrapper with getCssWithMedia

--- src/components/GridWrapper.js
+++ src/components/GridWrapper.js
@@ -1,28 +1,21 @@
-import styled from 'styled-components';
-import { media } from '../helpers/breakpoints';
-import { wrapperPadding, wrapperGridWidth } from '../helpers/defaultValue';
+import styled, { css } from 'styled-components';
+import { getCssWithMedia } from '../helpers';
 
-const GridWrapper = styled.div`
-  position: relative;
-  margin-left: auto;
-  margin-right: auto;
-  padding-left: ${({ padding: [desktop = wrapperPadding] = [] }) => desktop};
-  padding-right: ${({ padding: [desktop = wrapperPadding] = [] }) => desktop};
-  max-width: ${({ gridWidth: [desktop = wrapperGridWidth] = [] }) => desktop};
-  /* set width small than max-width */
-  width: 100%;
-  
-  ${media.tablet} {
-    padding-left: ${({ padding: [, tablet = ''] = [] }) => tablet}; 
-    padding-right: ${({ padding: [, tablet = ''] = [] }) => tablet};
-    max-width: ${({ gridWidth: [, tablet = ''] = [] }) => tablet};
+const GridWrapper = styled.div(
+  css`
+    position: relative;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+  `,
+  (props) => {
+    const styles = css`
+      padding-left: ${(gridSetting, breakpointKey) => gridSetting.padding[breakpointKey]};
+      padding-right: ${(gridSetting, breakpointKey) => gridSetting.padding[breakpointKey]};
+      max-width: ${(gridSetting, breakpointKey) => gridSetting.gridWidth[breakpointKey]};
+    `;
+    return getCssWithMedia(props, styles);
   }
-  
-  ${media.phone} {
-    padding-left: ${({ padding: [, , phone = ''] = [] }) => phone};
-    padding-right: ${({ padding: [, , phone = ''] = [] }) => phone};
-    max-width: ${({ gridWidth: [, , phone = ''] = [] }) => phone};
-  }
-`;
+);
 
 export default GridWrapper;
